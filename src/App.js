904fbd03bbbe9c81d5cdd9bd84b9413a33197b2b
Dani <admg17.isc@gmail.com>
2020-02-04 10:40:51 -0600

    Setting up our React app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,6 +19,64 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+
+
+import React from "react";
+import { Link } from "react-router-dom";
+//import { Navbar } from "react-bootstrap";
+import "./App.css";
+import Routes from  "./Routes";
+
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+/*
+function App(props) {
+  return (
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">MENU</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+      </Navbar>
+      <Routes />
+    </div>
+  );
+}
+*/
+
+function App(props) {
+  return (
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">MENU</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+          <LinkContainer to="/signup">
+              <NavItem>Subir Logo</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Generar PDF</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
     </div>
   );
 }
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Xml to PDF</h1>
+        <p>Guarda el logo de tu empresa y posteriormente genera tu factura PDF con el XML</p>
+      </div>
+    </div>
+  );
+}
